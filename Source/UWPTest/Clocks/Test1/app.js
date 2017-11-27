9ee1098222efcf5697b409545718c6923acb474d
QianFan Jiang <bucherjiang@msn.com>
2017-11-27 20:31:12 +0800

    optimize test script

--- Source/UWPTest/Clocks/Test1/app.js
+++ Source/UWPTest/Clocks/Test1/app.js
@@ -3,18 +3,29 @@ function draw() {
     var bWhite = JSBrush.createSolid(new JSColor("#ffffff"));
     var bBlack = JSBrush.createSolid(new JSColor("#000000"));
     img.Fill(bWhite);
-    JSFont.Install("DigitalDream.ttf");
-    var f = new JSFont("Digital Dream", 36);
+    //let f = new JSFont("Digital Dream", 60);
+    var f = new JSFont("Open 24 Display St", 60);
     var d = new Date();
-    img.DrawText(formatDate(d), f, bBlack, { x: 0, y: 0 });
-    //let imgT = JSImage.Load("img1.png");
-    //img.DrawImage(imgT, undefined, undefined, undefined, { x: 0, y: 90 });
+    img.DrawText(formatDate(d), f, bBlack, { x: 110, y: 80 });
+    var imgT = JSImage.Load("rickmorty.png");
+    img.DrawImage(imgT, undefined, undefined, undefined, { x: 0, y: 162 });
     img.SetOutput();
 }
 function formatDate(d) {
-    return ("0" + d.getHours().toString()).substr(1) + ":" +
-        ("0" + d.getMinutes().toString()).substr(1) + ":" +
-        ("0" + d.getSeconds().toString()).substr(1);
+    var hh = d.getHours().toString();
+    var mm = d.getMinutes().toString();
+    return addZero(hh) + ":" + addZero(mm) + ":" + addZero(d.getSeconds().toString());
 }
-function setup() { }
+function addZero(s) {
+    if (s.length < 2) {
+        return "0" + s;
+    }
+    else {
+        return s;
+    }
+}
+function setup() {
+    JSFont.Install("Open 24 Display St.ttf");
+    JSFont.Install("simhei.ttf");
+}
 //# sourceMappingURL=app.js.map
--- Source/UWPTest/Clocks/Test1/app.ts
+++ Source/UWPTest/Clocks/Test1/app.ts
@@ -5,22 +5,35 @@
 
 
     img.Fill(bWhite);
-    JSFont.Install("DigitalDream.ttf");
-    let f = new JSFont("Digital Dream", 36);
+    
+    
 
-
+    //let f = new JSFont("Digital Dream", 60);
+    let f = new JSFont("Open 24 Display St", 60);
     let d = new Date();
-    img.DrawText(formatDate(d), f, bBlack, { x: 0, y: 0 });
+    img.DrawText(formatDate(d), f, bBlack, { x: 110, y: 80 });
 
-    //let imgT = JSImage.Load("img1.png");
-    //img.DrawImage(imgT, undefined, undefined, undefined, { x: 0, y: 90 });
-
+    let imgT = JSImage.Load("rickmorty.png");
+    img.DrawImage(imgT, undefined, undefined, undefined, { x: 0, y: 162 });
+    
     img.SetOutput();
 }
 
 function formatDate(d: Date): string {
-    return ("0" + d.getHours().toString()).substr(1) +":"+
-        ("0" + d.getMinutes().toString()).substr(1) +":"+
-        ("0" + d.getSeconds().toString()).substr(1);
+    let hh = d.getHours().toString();
+    let mm = d.getMinutes().toString();
+    return addZero(hh) + ":" + addZero(mm)+":"+addZero(d.getSeconds().toString());
 }
-function setup() {}
+function addZero(s: string) {
+    if (s.length < 2) {
+        return "0" + s;
+    }
+    else {
+        return s;
+    }
+}
+function setup() {
+    JSFont.Install("Open 24 Display St.ttf");
+    JSFont.Install("simhei.ttf");
+    
+}
