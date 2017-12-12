b22d576b39fa250a06111177434ea7c48cbac7a3
QianFan Jiang <bucherjiang@msn.com>
2017-12-12 19:34:40 +0800

    minor code fix

--- Source/UWPTest/MainPage.xaml.cs
+++ Source/UWPTest/MainPage.xaml.cs
@@ -37,16 +37,16 @@ namespace UWPTest
             this.Loaded += MainPage_Loaded;
         }
 
-        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             refreshPacks();
             ImageSourceRenderer imgRenderer = new ImageSourceRenderer();
             this.imgResult.DataContext = imgRenderer.Data;
             render.Renderers.Add(new CombinedRendererItem(imgRenderer));
             WaveShareEink32Renderer einkRenderer = new WaveShareEink32Renderer();
-            render.Renderers.Add(new CombinedRendererItem(einkRenderer,false));
+            render.Renderers.Add(new CombinedRendererItem(einkRenderer, false));
             RemoteRenderer remoteRenderer = new RemoteRenderer("192.168.0.220");
-            render.Renderers.Add(new CombinedRendererItem(remoteRenderer,false));
+            render.Renderers.Add(new CombinedRendererItem(remoteRenderer, false));
             lstRenders.ItemsSource = render.Renderers;
             var xinzhi = new XinzhiWeatherForcast("gxs3ezcux67dzvqa", "shanghai");//replace the key with your own, this is for my development only
             xinzhi.Start();
