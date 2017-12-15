c204142aac4464437830b5aa359200ed36cf075f
QianFan Jiang <bucherjiang@msn.com>
2017-12-15 18:59:27 +0800

    replace broken UWPTest project with UWPTest1

--- Source/UWPTest1/App.xaml.cs
+++ Source/UWPTest1/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace UWPTest
+namespace UWPTest1
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- Source/UWPTest1/MainPage.xaml.cs
+++ Source/UWPTest1/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SmartClock.InfoProviders.XinZhiWeatherInfoProvider;
+using SmartClock.JSClockManager;
+using SmartClock.NetcoreRenderer;
+using SmartClock.UWPRenderer;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,14 +16,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using SmartClock.InfoProviders.XinZhiWeatherInfoProvider;
-using SmartClock.UWPRenderer;
-using SmartClock.NetcoreRenderer;
-using SmartClock.JSClockManager;
-using System.Threading.Tasks;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace UWPTest
+namespace UWPTest1
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -65,7 +65,7 @@ namespace UWPTest
             btnLoad.IsEnabled = false;
             manager.StartClock(CLOCK_NAME, info.FolderName);
             btnLoad.IsEnabled = true;
-            
+
         }
     }
 }
